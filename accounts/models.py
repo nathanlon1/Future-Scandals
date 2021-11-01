0fe2f777535d9ad67aab45ab0ef08295aba17d4f
Nathan Hanlon <nathan.hanlon@umconnect.umt.edu>
2021-11-01 13:57:05 -0600

    Clarified comments in the save overriding metho for default value for displayName

--- accounts/models.py
+++ accounts/models.py
@@ -6,6 +6,7 @@ class CustomUser(AbstractUser):
     
     # Manually override save function
     # https://stackoverflow.com/questions/4380879/django-model-field-default-based-off-another-field-in-same-model
+    # https://docs.djangoproject.com/en/3.2/topics/db/models/#overriding-model-methods (More accurate and clear)
     
     def save(self, *args, **kwargs):
         if not self.displayName:
