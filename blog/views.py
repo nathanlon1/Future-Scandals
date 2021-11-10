f265afb15da0a43302388abc5f149c31138dabf2
Nathan Hanlon <nathan.hanlon@umconnect.umt.edu>
2021-11-10 14:48:50 -0700

    Added Templates to settings.py, added half broken blog detail view.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.views.generic import DetailView
+from .models import Post
 
-# Create your views here.
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = 'blog_detail.html'
